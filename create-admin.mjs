a1460ad28364c451314430f550850f047c76d76e
Chihab_ekwip <chihab@ekwip.ma>
2026-01-04 14:20:10 +0100

    chore: create admin user ch.jabri@supermedia.ma

---
+++ create-admin.mjs
@@ -0,0 +1,4 @@
+// This script was used to create an admin user.
+// User created: ch.jabri@supermedia.ma (Owner role)
+// Script cleared for security.
+console.log('Admin user already created. See git history if needed.');
